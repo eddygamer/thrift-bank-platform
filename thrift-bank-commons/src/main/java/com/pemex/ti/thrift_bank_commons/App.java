7d4b9c84784c970fa95223d7cd6821e739e7e830
eddygamer <edcgamer@gmail.com>
2018-11-25 02:55:55 -0600

    Setting platform base

---
+++ thrift-bank-commons/src/main/java/com/pemex/ti/thrift_bank_commons/App.java
@@ -0,0 +1,13 @@
+package com.pemex.ti.thrift_bank_commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ thrift-bank-sdk/src/main/java/com/pemex/ti/thrift_bank_sdk/App.java
@@ -0,0 +1,13 @@
+package com.pemex.ti.thrift_bank_sdk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
